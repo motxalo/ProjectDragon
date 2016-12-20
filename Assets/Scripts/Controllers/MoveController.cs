6ded588d169e2af9074de1d62dbc0f13030e9ddf
Dani <daniandia@gmail.com>
2016-12-20 21:19:14 +0100

    prueba jugable

---
+++ Assets/Scripts/Controllers/MoveController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+	private float distance = 100f;
+	private bool canRaycast = true;
+
+	public Transform aim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(canRaycast) DoRayCast();
+	}
+
+	private DisplacementPoint  lastDestPoint;
+
+	void DoRayCast(){
+		
+		RaycastHit  hit; 
+
+		if (Physics.Raycast(transform.position, Camera.main.transform.forward,out hit, distance)){
+			aim.position = hit.point;
+			if ( hit.collider.gameObject.tag == "DisplacementPoint" ){
+				if(lastDestPoint){
+					lastDestPoint.KillKids(hit.collider.name);
+				}
+				lastDestPoint = hit.collider.GetComponent<DisplacementPoint>();
+				Debug.Log(hit.collider.name);
+				float dispTime  = lastDestPoint.DoMovement(transform);
+				canRaycast = false;
+				aim.GetComponent<Renderer>().enabled = false;
+				Invoke("ActivateRaycast", dispTime );
+			}
+		}
+		else{
+			aim.position = transform.position + Camera.main.transform.forward*distance;
+		}
+	}
+
+	void ActivateRaycast(){
+		aim.GetComponent<Renderer>().enabled = true;
+		canRaycast = true;
+	}
+}
---
+++ Assets/Scripts/Movement/DisplacementPoint.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementPoint : MonoBehaviour {
+	public bool startActive = false;
+	public LeanTweenType movemntType = LeanTweenType.easeInOutBounce;
+	public float movementTime = 2f;
+	public bool keepY = true;
+
+	public GameObject[] linkedPoints;
+	// Use this for initialization
+	void Start () {
+		if(!startActive){
+			Deactivate();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public float DoMovement(Transform _player){
+		Vector3 destiny = transform.position;
+		if ( keepY ) destiny.y=_player.position.y;
+		LeanTween.move(_player.gameObject, destiny, movementTime ).setEase(movemntType);
+		Deactivate();
+		int i = 0;
+		for (i = 0; i < linkedPoints.Length; i++){
+				linkedPoints[i].SendMessage("Activate", SendMessageOptions.DontRequireReceiver);
+		}
+		
+		return movementTime;
+	}
+
+	public void Activate(){
+		GetComponent<Renderer>().enabled = true;
+		GetComponent<Collider>().enabled = true;
+	}
+		
+
+	public void Deactivate(){
+		GetComponent<Collider>().enabled = false;
+		GetComponent<Renderer>().enabled = false;
+	}
+
+
+	public void KillKids(string _name){
+		int i = 0;
+		for (i = 0; i < linkedPoints.Length; i++){
+			if(gameObject.name != _name)
+				linkedPoints[i].SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
